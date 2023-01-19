5d0247e25426f63cf53fd5b25068f899a2c370ac
FiresoftGH <most.chuti@gmail.com>
2023-01-19 11:50:36 +0700

    Second Commit

--- sample_0.asm
+++ sample_0.asm
@@ -1,8 +1,8 @@
     .text
     .globl main
 
-# Comment 
+# "addu" add unsigned "li" load integer
 main:
-    li  $t0, 0x2
-    li  $t1, 0x3
+    li  $t0, -0x2
+    li  $t1, -0x3
     addu $t2, $t0, $t1
---
+++ sample_2.asm
@@ -0,0 +1,15 @@
+    .data
+n:  .word   0x7
+m:  .word   0x8
+r:  .space  12
+q:  .word   0x9
+
+    .text
+    .globl  main
+
+main:
+    lw		$t0, n
+    lw      $t1, m
+    addu    $t2, $t0, $t1
+    sw		$t2, 4(r)
+    lw      $t3, q
