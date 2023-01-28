53707259a1d6a99b2b5f1a483b5cb193428d63d6
FiresoftGH <most.chuti@gmail.com>
2023-01-28 14:22:40 +0700

    Updating Assignment_2.asm

--- MIPS_Assembly_Intro/Assigment_2.asm
+++ MIPS_Assembly_Intro/Assigment_2.asm
@@ -2,12 +2,13 @@
 x:      .asciiz "Enter your number here: "
 y:      .asciiz "Your answer is: "
 z:      .asciiz "The number entered is lesser than 0. "
-q:      .asciiz " Overflow: "
+q:      .asciiz "Overflow Error "
 v:      .asciiz "\n"
         .text
         .globl main
 
 main:
+    # Print the introduction message and ask the user for a number
     li          $v0, 4
     la		    $a0, x
     syscall
@@ -18,11 +19,15 @@ main:
     move        $t0, $v0
 
     slt		    $t3, $t0, 0
-
     beq         $t3, 1, error
+
+    # From testing, the factorial of number greater than 12 exceeds the 32 bit unsigned integer limit.
+    sgt         $t3, $t0, 12
+    beq         $t3, 1, overflow 
     li          $t1, 1
 
 loop:
+    # The main loop that calculates the factorial.
     beq         $t0, 1, done
     multu       $t1, $t0
     mfhi        $t2
@@ -31,6 +36,7 @@ loop:
     j           loop
 
 done:
+    # Exiting the loop and shows the answer and ask for another input
     li          $v0, 4
     la          $a0, y
     syscall
@@ -40,20 +46,13 @@ done:
     syscall
 
     li          $v0, 4
-    la          $a0, q
-    syscall
-
-    move        $a0, $t2
-    li          $v0, 1
-    syscall
-
-    li          $v0, 4
     la          $a0, v
-    syscall       
+    syscall
 
-    j           main
+    j main
 
 error:
+     # If the number is lesser than zero, the error message will be shown
      li         $v0, 4
      la         $a0, z
      syscall
@@ -64,7 +63,17 @@ error:
 
      j          main
     
-overflow:
+overflow: 
+     # If the input is greater than 12, this error will be shown
+     li         $v0, 4
+     la         $a0, q
+     syscall
+
+     li         $v0, 4
+     la         $a0, v
+     syscall
+
+     j          main
      
 
             
